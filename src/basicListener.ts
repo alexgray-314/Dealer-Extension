2c775b821aaf9afaf769b5e04cdd983c088c72d9
Alex Gray <alexjgray@live.co.uk>
2026-02-04 13:13:01 +0000

    Variable declaration checking will also check for loops

--- src/basicListener.ts
+++ src/basicListener.ts
@@ -1,5 +1,5 @@
 import { dealListener } from "./parser/dealListener";
-import { AssignContext, DefinitionContext, MoveContext, SourceContext, VariableContext } from "./parser/dealParser";
+import { AssignContext, DefinitionContext, ForContext, MoveContext, SourceContext, VariableContext } from "./parser/dealParser";
 import * as vscode from "vscode";
 
 export class BasicListener implements dealListener {
@@ -12,6 +12,11 @@ export class BasicListener implements dealListener {
         this.output = outputChannel;
         this.variables = [];
         this.diagnostics = diagnostics;
+    }
+
+    enterFor(ctx: ForContext){
+        const id = ctx.ID().text;
+        this.variables.push(id);
     }
 
     enterDefinition(ctx: DefinitionContext) {
